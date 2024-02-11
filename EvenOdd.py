26be723d5828c9edeb77a4b69c883a264d38a27f
Zulfiqar Alam <zulfiqaralam651@gmail.com>
2024-02-11 20:18:28 +0500

    Initail stage of myproject

---
+++ EvenOdd.py
@@ -0,0 +1,6 @@
+Number=int(input("Enter any Number: "))
+
+if Number%2==0:
+  print("Number is Evennn")
+else:
+  print("Number is Odd")
---
+++ cal.py
@@ -0,0 +1,15 @@
+a=10
+b=20
+
+op=input("Enter Operator :")[0]
+
+if op=='+':
+    print(a+b)
+elif op=='-':
+    print(a-b)
+elif op=='/':
+    print(a/b)
+elif op =='*':
+    print(a*b)
+else:
+    print("Error:Invalid operator")
