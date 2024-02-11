c15797b7eac7413b15533f35731b8e353e6d8591
Zulfiqar Alam <zulfiqaralam651@gmail.com>
2024-02-11 21:19:38 +0500

    First Commit

---
+++ Login.py
@@ -0,0 +1,10 @@
+u_name ="Zulfi"
+u_pass= "123"
+
+name=input("Enter User Name:")
+password=input("Enter user Password:")
+
+if name==u_name and password==u_pass:
+    print("LogIn Successfully")
+else:
+    print("Error")
